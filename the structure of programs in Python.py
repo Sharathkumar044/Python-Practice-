b65224f0e9e4cb14d6ee1df7bcb367a00db109b6
sharathkumar044 <sharathkumar044@gmail.com>
2025-01-19 22:04:53 +0530

    latest commit

---
+++ the structure of programs in Python.py
@@ -0,0 +1,9 @@
+import math  # math is a module
+
+x = 10  # Assignment statement
+print(x)  # Function call statement
+
+y = 10 + 5  # 10 + 5 is an expression
+
+
+z = "hello" * 3  # "hellohellohello" is the resulting object (string)
