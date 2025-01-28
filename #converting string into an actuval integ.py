357980daf5945d64a03c226525a4636ee959e6ad
sharathkumar044 <sharathkumar044@gmail.com>
2025-01-28 14:04:51 +0530

    new addition

---
+++ #converting string into an actuval integ.py
@@ -0,0 +1,4 @@
+#converting string into an actuval integer.
+string = ("123")
+integer = int(string)
+print (string * 56)
--- Test.py
+++ Test.py
@@ -1,4 +1,13 @@
+##Converting String Case
+
 name = "sharath"
 print (name .upper())
 
 print (name .lower())
+
+##To remove whitespace from both the left and the right sides of the
+##string at the same time, use .strip() :
+name1 = "    sharath kumar   "
+print (name1)
+
+print (name1.strip())
